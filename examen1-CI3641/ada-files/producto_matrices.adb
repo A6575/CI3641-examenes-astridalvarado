8adb5b873db24c2569d2d924904b213a9d210f0d
A6575 <astridaalvaradoa@gmail.com>
2023-10-15 00:32:27 -0400

    changes

--- examen1-CI3641/ada-files/producto_matrices.adb
+++ examen1-CI3641/ada-files/producto_matrices.adb
@@ -1,42 +1,61 @@
 -- Pregunta 1, inciso (b.ii)
 
+-- Importaciones de los paquetes Text_IO y Numerics.Real_Arrays
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Numerics.Real_Arrays; use Ada.Numerics.Real_Arrays;
+
+-- Declaracion del procedimiento Producto_Matrices
 procedure Producto_Matrices is
+    -- Funcion para el llenado de una matriz cuadrada a partir del numero de filas que tendra y un string ingresado
     function Filling_Matrix_By_Space (row:Integer) return Real_Matrix is
-        num_of_row:Natural:=0;
-        Matrix: Real_Matrix(1..row,1..row):=(others => (others => 0.0));
+        num_of_row:Natural:=0;                                              -- contador del numero de filas ingresadas
+        Matrix: Real_Matrix(1..row,1..row):=(others => (others => 0.0));    -- Matriz de tamano row x row inicializado en 0.0
     begin
-        while num_of_row /= row loop
-            Put("Ingrese la fila nro"&Integer'Image(num_of_row+1)&" separando los numeros por espacios: ");
-            declare
-                rows: String := Get_Line;
-                len_of_num: Natural := 0;
-                offset: Natural:= 0;
-                num_of_col: Natural:= 0;
-            begin
-                for J in rows'Range loop
-                    if rows(J..J) = " " then
-                        if num_of_col > row then
-                            Put_Line("El numero de columnas debe coincidir con el numero de filas.");
-                        end if;
-                        Matrix(num_of_row+1, num_of_col+1) := Float(Integer'Value(rows(offset+1..len_of_num+offset)));
-                        offset:= offset + len_of_num + 1;
-                        num_of_col := num_of_col+1;
-                        len_of_num := 0;
-                    else
-                        len_of_num := len_of_num + 1;
-                        if J = rows'Length then
+        -- En caso de que se ingrese un tamano negativo se reporta un mensaje de error
+        if row < 0 then
+            Put_Line("Error: la dimension de la matriz no puede ser un numero negativo");
+        else 
+            -- mientras que el numero de filas ingresadas sea distinto a la cantidad objetivo de estas
+            while num_of_row /= row loop
+                -- se le pide al usuario ingresar la fila siguiendo el formato "n1 n2 n3 ..."
+                Put("Ingrese la fila nro"&Integer'Image(num_of_row+1)&" separando los numeros por espacios: ");
+                declare
+                    -- declaracion de variables
+                    rows: String := Get_Line;   -- filas ingresadas por el usuario en formato string
+                    len_of_num: Natural := 0;   -- contador para saber la longitud del numero actual, esto es, saber cuantos digitos (incluyendo el signo) tiene el numero
+                    offset: Natural:= 0;        -- desplazamiento por el string
+                    num_of_col: Natural:= 0;    -- contador para el numero de columnas
+                begin
+                    -- se recorre el string dado 
+                    for J in rows'Range loop
+                        -- y si en la posicion actual se encuentra un espacio
+                        if rows(J..J) = " " then
+                            -- se extrae del string el numero ingresado utilizando la longitud calculada del mismo y se guarda en la matriz
                             Matrix(num_of_row+1, num_of_col+1) := Float(Integer'Value(rows(offset+1..len_of_num+offset)));
-                        end if;                    
-                    end if;
-                end loop;
-            end;
-            num_of_row := num_of_row + 1;
-        end loop;
+                            
+                            offset:= offset + len_of_num + 1;   -- se aumenta el desplazamiento necesario para continuar con la lectura de la fila
+                            num_of_col := num_of_col+1;         -- se aumenta el numero de columnas
+                            len_of_num := 0;                    -- y se resetea el tamano del numero actual
+                        else
+                            -- en caso de no encontrar un espacio en el string, se aumenta la longitud de numero
+                            len_of_num := len_of_num + 1;
+
+                            -- Si se recorrio todo el string, entonces se almacena el ultimo numero sobrante
+                            if J = rows'Length then
+                                Matrix(num_of_row+1, num_of_col+1) := Float(Integer'Value(rows(offset+1..len_of_num+offset)));
+                            end if;                    
+                        end if;
+                    end loop;
+                end;
+                -- Se aumenta la cantidad de filas ingresadas
+                num_of_row := num_of_row + 1;
+            end loop;
+        end if;
+        -- Finalmente, se retorna la matriz
         return Matrix;
     end Filling_Matrix_By_Space;
-
+    
+    -- procedimiento para la impresion de una matriz
     procedure Show_Matrix (M: Real_Matrix) is
     begin
         for I in M'Range(1) loop
@@ -47,14 +66,21 @@ procedure Producto_Matrices is
         end loop;
     end Show_Matrix;
 begin
+    -- se le pide al usuario ingresar las dimensiones de la matriz. Al ser cuadrada, basta con ingresar un numero
     Put("Ingrese la dimesion de la matriz cuadrada: ");
 
     declare
-        dimesion_of_matrix:Integer := Integer'Value(Get_Line);
-        M:Real_Matrix:=Filling_Matrix_By_Space(dimesion_of_matrix);
+        dimesion_of_matrix:Integer := Integer'Value(Get_Line);          -- variable para almacenar el numero ingresado
+        M:Real_Matrix:=Filling_Matrix_By_Space(dimesion_of_matrix);     -- matriz ingresada
     begin
-        Put_Line("El resultado es: ");
+        if dimesion_of_matrix < 0 then
+            --finalizacion del programa por error reportado
+            Put("");
+        else
+            -- Finalmente, se imprime el resultado
+            Put_Line("El resultado es: ");
 
-        Show_Matrix(M*Transpose(M));
+            Show_Matrix(M*Transpose(M));
+        end if;
     end;
 end Producto_Matrices;
--- examen1-CI3641/ada-files/rotar_string.adb
+++ examen1-CI3641/ada-files/rotar_string.adb
@@ -8,42 +8,50 @@ with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 -- Declacion del procedimiento Rotar_String
 procedure Rotar_String is
     -- Declaraciones de variables usadas en el procedimiento:
-    string_to_rotate: String := Get_Line;                       -- String ingresado por el usuario
-    num_for_rotation: Integer := Integer'Value(Get_Line);       -- Numero ingresado por el usuario
-    len_of_string: Natural := string_to_rotate'Length;          -- longitud del string ingresado
     mod_num_len: Integer:= 0;                                   -- Variable para almacenar el valor resultante de 'num_of_rotation mod len_of_string' 
-    rotated_string:String(1..len_of_string);                   -- Variable para almacenar el string una vez haya sido rotado
 begin
-    -- Se verifica que el numero para realizar la rotacion sea un numero no-negativo
-    if num_for_rotation<0 then
-       Put_Line("El numero ingresado debe ser mayor o igual a 0");
-    else
-        -- En caso de que el usuario ingrese la cadena vacia "", se retorna la misma
-        if string_to_rotate'Length = 0 then
-            Put_Line(string_to_rotate);
-        else
-            -- se calcula el modulo del numero ingresado para las rotaciones 
-            -- con respecto a la longitud del string ingresado para saber el 
-            -- tamano se las subcadenas para realizar la rotacion
-            mod_num_len := num_for_rotation mod len_of_string;
-
-            -- en caso de que este valor sea 0, es decir, es multiplo de la longitud del string
-            -- entonces se imprime la caneda original
-            if (mod_num_len = 0) then
-                Put_Line(string_to_rotate);
+    Put("Ingrese la palabra que desee rotar: ");
+    declare
+        string_to_rotate: String := Get_Line;                       -- String ingresado por el usuario
+        len_of_string: Natural := string_to_rotate'Length;          -- longitud del string ingresado
+        rotated_string:String(1..len_of_string);                   -- Variable para almacenar el string una vez haya sido rotado
+    begin
+        Put("Ingrese la cantidad de caracteres a rotar de su palabra: ");
+        declare
+            num_for_rotation: Integer := Integer'Value(Get_Line);       -- Numero ingresado por el usuario
+        begin
+             -- Se verifica que el numero para realizar la rotacion sea un numero no-negativo
+            if num_for_rotation<0 then
+                Put_Line("El numero ingresado debe ser mayor o igual a 0");
             else
-                -- se selecciona las primeros mod_num_len caracteres del string ingresado y se
-                -- almacenan en los ultimos mod_num_len caracteres de rotated_string. De esta forma
-                -- se hacer la rotacion correspondiente
-                rotated_string (len_of_string-(mod_num_len-1)..len_of_string):= string_to_rotate(1..mod_num_len);
-                
-                -- Luego, se selecciona el resto del string ingresado y se posiciona al principio del
-                -- string resultado
-                rotated_string (1.. len_of_string-mod_num_len) := string_to_rotate(mod_num_len+1..len_of_string);
+                -- En caso de que el usuario ingrese la cadena vacia "", se retorna la misma
+                if string_to_rotate'Length = 0 then
+                    Put_Line(string_to_rotate);
+                else
+                    -- se calcula el modulo del numero ingresado para las rotaciones 
+                    -- con respecto a la longitud del string ingresado para saber el 
+                    -- tamano se las subcadenas para realizar la rotacion
+                    mod_num_len := num_for_rotation mod len_of_string;
 
-                -- finalmente, se imprime el string resultante
-                Put_Line(rotated_string);
+                    -- en caso de que este valor sea 0, es decir, es multiplo de la longitud del string
+                    -- entonces se imprime la caneda original
+                    if (mod_num_len = 0) then
+                        Put_Line(string_to_rotate);
+                    else
+                        -- se selecciona las primeros mod_num_len caracteres del string ingresado y se
+                        -- almacenan en los ultimos mod_num_len caracteres de rotated_string. De esta forma
+                        -- se hacer la rotacion correspondiente
+                        rotated_string (len_of_string-(mod_num_len-1)..len_of_string):= string_to_rotate(1..mod_num_len);
+                        
+                        -- Luego, se selecciona el resto del string ingresado y se posiciona al principio del
+                        -- string resultado
+                        rotated_string (1.. len_of_string-mod_num_len) := string_to_rotate(mod_num_len+1..len_of_string);
+
+                        -- finalmente, se imprime el string resultante
+                        Put_Line(rotated_string);
+                    end if;
+                end if;
             end if;
-        end if;
-    end if;
+        end;
+    end; 
 end Rotar_String;
