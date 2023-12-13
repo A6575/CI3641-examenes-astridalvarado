84291227d0e8a5430943a46db4fc03407457156e
A6575 <astridaalvaradoa@gmail.com>
2023-12-13 10:20:18 -0400

    comentarios para la 4

--- examen3-CI3641/python_files/pregunta_4/main.py
+++ examen3-CI3641/python_files/pregunta_4/main.py
@@ -1,10 +1,11 @@
 import vtable as vt
 from itertools import takewhile, dropwhile
+
 def main():
     # se le muestra al usuario las opciones disponibles
     print(""" OPERACIONES DISPONIBLES:
         -- CLASS <tipo> [<nombre>]: Define un nuevo tipo que poseerá métodos con nombres establecidos en la lista proporcionada.
-                       El <tipo> puede ser:
+                                    El <tipo> puede ser:
                                 • Un nombre, que establece un tipo que no hereda de ningún otro.
                                 • Una expresión de la forma <nombre> : <super>, que establece el nombre del tipo y el 
                                                                        hecho de que este tipo hereda del tipo con nombre <super>.
@@ -29,41 +30,47 @@ def main():
         
          # Si la operacion ingresada es CLASS
         if "CLASS" in command:
+            # si existe : en la lista de datos, se considera que el tipo tiene una superclase
             if ":" in datatype_as_list:
+                # se separan los datos en 3 partes: nombre, superclase y atributos
                 subclase = datatype_as_list[0]
                 superclase = datatype_as_list[2]
                 atributos = datatype_as_list[3:]
+                # se definen las clases
                 manejador_global.definir_clase(subclase, atributos, superclase)
-
+                
+                # si existe algun error, se reporta el mismo
                 if manejador_global.error_clase != "" or manejador_global.error_superclase != "" or manejador_global.error_atributos != "":
                     print("\n".join([manejador_global.error_clase, manejador_global.error_superclase, manejador_global.error_atributos]))
-                    break
-
-                print(f"Se ha definido una nueva subclase {subclase} de {superclase} con atributos: "+", ".join(atributos))
+                else:
+                    print(f"Se ha definido una nueva subclase {subclase} de {superclase} con atributos: "+", ".join(atributos))
             else:
+                # si no existe : en la lista de datos, se considera que el tipo no tiene una superclase
+                # se separan los datos en 2 partes: nombre y atributos
                 clase = datatype_as_list[0]
                 atributos = datatype_as_list[1:]
+                # se definen las clases
                 manejador_global.definir_clase(clase, atributos)
-
+                
+                 # si existe algun error, se reporta el mismo
                 if manejador_global.error_clase != "" or manejador_global.error_superclase != "" or manejador_global.error_atributos != "":
                     print("\n".join([manejador_global.error_clase, manejador_global.error_superclase, manejador_global.error_atributos]))
-                    break
-
-                print(f"Se ha definido una nueva clase {clase} con atributos: "+", ".join(atributos))
+                else:            
+                    print(f"Se ha definido una nueva clase {clase} con atributos: "+", ".join(atributos))
     
         # si la operacion ingresada en DESCRIBIR
         elif "DESCRIBIR" in command and len(datatype_as_list) == 1:
+            # se obtiene la descripcion de la clase
             metodos_clase = manejador_global.describir_clase(datatype_as_list[0])
             
+            # si se encuentra que no existe la clase a describir, se reporta error
             if manejador_global.error_clase != "":
                 print(manejador_global.error_clase)
-                break
-
-            print(f"TABLA DE METODOS VIRTUALES DE {datatype_as_list[0]}")
-            for metodos, clases in metodos_clase:
-                for met in metodos:
-                    print(f"{met} -> {clases} :: {met}")
-            
+            else:
+                print(f"---- TABLA DE METODOS VIRTUALES DE {datatype_as_list[0]} ----")
+                for metodos, clases in metodos_clase:
+                    for met in metodos:
+                        print(f"\t\t{met} -> {clases} :: {met}")
         else:
             print("\nERROR: Operacion invalida.\n")
 
--- examen3-CI3641/python_files/pregunta_4/vtable.py
+++ examen3-CI3641/python_files/pregunta_4/vtable.py
@@ -1,46 +1,65 @@
+# Clase para manejas la tablas vituales de metodos
 class ManejadorVtable():
+    # construcctor de la clase 
     def __init__(self) -> None:
-        self.manejador_vtable = {}
+        self.manejador_vtable = {}      # diccionario donde se almacenaran las clases y sus metodos
+        self.error_atributos = ""       # mensaje de error correspondiente a los atributdos de las clases
+        self.error_clase = ""           # mensaje de error correspondiente a la clase
+        self.error_superclase = ""      # mensaje de error correspondiente a la superclase
+
+    # funcion para definir nuevas clases 
+    def definir_clase(self, clase: str, atributos: list, superclase: str = None):
+        # limpiar mensajes de error
         self.error_atributos = ""
         self.error_clase = ""
         self.error_superclase = ""
 
-    def definir_clase(self, clase, atributos, superclase = None):
-        self.error_atributos = ""
-        self.error_clase = ""
-        self.error_superclase = ""
-
+        # validar atributos para que no existan nombres repetidos
         metodos_clase = list(map(atributos.count, set(atributos)))
         if any([x for x in metodos_clase if x > 1]):
             self.error_atributos = "ERROR: existen metodos con nombres repetidos en la definicion de la clase"
+        # verificar que una clase ya existente no pueda ser redefinida
         if clase in self.manejador_vtable.keys():
             self.error_clase = f"ERROR: la clase {clase} ya existe."
+        # verificar que la superclase exista
         if superclase != None and superclase not in self.manejador_vtable.keys():
             self.error_superclase = f"ERROR: la superclase {superclase} no existe."
         
+        # si no se encontró ningún error, se almacena la clase en el diccionario
         if self.error_atributos == "" and self.error_clase == "" and self.error_superclase == "":
             self.manejador_vtable[clase] = [atributos, superclase]
     
-    def describir_clase(self, clase):
-        self.error_clase = ""
-        metodos_clase = []
+    def describir_clase(self, clase: str):
+        self.error_clase = ""   # limpiar mensaje de error
+        metodos_clase = []      # lista donde se almacenaran los metodos de la clase
 
+        # validar que la clase exista
         if clase not in self.manejador_vtable.keys():
             self.error_clase = f"la clase {clase} no ha sido definida"
         else:
+            # obtener los metodos de la clase y sus superclases, de tenerlas
             vtable_clase = self.manejador_vtable[clase]
+            # se agrega a la lista los metodos de la clase como un conjunto, y la clase de donde se obtuvo
             metodos_clase.append([set(vtable_clase[0]), clase])
 
+            # mientras que la superclase no sea None
             while vtable_clase[1] != None:
+                # se obtiene el nombre de la superclase
                 superclase = vtable_clase[1]
+                # se obtiene los metodos de la superclase
                 vtable_clase = self.manejador_vtable[superclase]
+                # se realiza una transformacion de los metodos encontrados en la lista metodos_clase
                 conjuntos_metodos_clase = [set(metodos[0]) for metodos in metodos_clase]
+                # se obtiene el conjunto de metodos de la superclase
                 conjunto_metodos_actual = set(vtable_clase[0])
 
+                # y para todos los conjuntos de los metodos de la clase
                 for u_metodos in conjuntos_metodos_clase:
+                    # siempre que exista intersección, se realiza la diferencia de conjuntos
                     if any(u_metodos & conjunto_metodos_actual):
                         conjunto_metodos_actual -= u_metodos
-
+                
+                # finalmente, se agrega a la lista
                 metodos_clase.append([conjunto_metodos_actual, superclase])
                 
         return metodos_clase
