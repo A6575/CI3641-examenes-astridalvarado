aaf3ae26565dab0d92831b60d39eefea5d88729f
A6575 <astridaalvaradoa@gmail.com>
2023-12-13 14:08:20 -0400

    comentarios pregunta 1

--- examen3-CI3641/c++_files/grafos.cpp
+++ examen3-CI3641/c++_files/grafos.cpp
@@ -1,3 +1,4 @@
+// librerias necesarias para el funcionamiento del programa
 #include <iostream>
 #include <stack>
 #include <queue>
@@ -6,22 +7,32 @@
 
 using namespace std;
 
+// definicion del tipo de datos Grafo
+// la lista de adyacencia fue implementada como un mapa de int a vector de int
+// esto con el fin de poder tener nodos con valores distintos y no necesariamente
+// secuenciales
 struct Grafo{
     map<int, vector<int> > lista_adyacencia;
 };
 
+// definicion de la clase Busqueda
 class Busqueda{
     public:
+        // Destructor por defecto
+        virtual ~Busqueda() = default;
+        // Metodo que debe ser implementado en las clases derivadas
         virtual int buscar(int H, int D, const Grafo &G) = 0;
 };
 
+// definicion de la clase derivada DFS
 class DFS : public Busqueda {
 public:
+    // Metodo que implementa el algoritmo DFS iterativo mediante el uso de un stack
     int buscar(int H, int D, const Grafo &graph) override{
         stack<int> stack;
         map<int, bool> visitado;
         vector<int> camino;
-        // Inicializar con false
+        // Inicializar con false todos los nodos del grafo en el mapa visitado
         for(auto& par : graph.lista_adyacencia) {
          visitado[par.first] = false; 
         }
@@ -32,10 +43,13 @@ public:
             int nodo = stack.top();
             stack.pop();
 
+            // Si el nodo es el destino de busqueda, retornar la cantidad de nodos explorados
+            // si contar el nodo de destino
             if (nodo == D) {
                 return camino.size(); // Cantidad de nodos explorados
             }
 
+            // Si el nodo no ha sido visitado, marcarlo como visitado y agregarlo al camino
             if (!visitado[nodo]) {
                 visitado[nodo] = true;
                 camino.push_back(nodo);
@@ -46,28 +60,37 @@ public:
                 }
             }
         }
-        return -1; // Nodo H no alcanzable desde D
+        return -1; // Nodo D no alcanzable desde H
     }
 };
 
+// definicion de la clase derivada BFS
 class BFS : public Busqueda {
 public:
+    // Metodo que implementa el algoritmo BFS iterativo mediante el uso de una cola
     int buscar(int H, int D, const Grafo& graph) override{ 
         queue<int> cola;
         map<int, bool> visitado;
         vector<int> camino;
-        // Inicializar con false
+
+        // Inicializar con false todos los nodos del grafo en el mapa visitado
         for(auto& par : graph.lista_adyacencia) {
          visitado[par.first] = false; 
         }
         
         cola.push(H);
+
         while (!cola.empty()) {
             int nodo = cola.front();
             cola.pop();
+
+            // Si el nodo es el destino de busqueda, retornar la cantidad de nodos explorados
+            // si contar el nodo de destino
             if (nodo == D) {
                 return camino.size(); // Cantidad de nodos explorados
             }
+
+            // Si el nodo no ha sido visitado, marcarlo como visitado y agregarlo al camino
             if (!visitado[nodo]) {
                 visitado[nodo] = true;
                 camino.push_back(nodo);
@@ -78,16 +101,18 @@ public:
                 }
             }
         }
-        return -1; // Nodo H no alcanzable desde D
+        return -1; // Nodo D no alcanzable desde H
     }
-}; 
+};
 
+/********** PROGRAMA PRINCIPAL **********/
 int main(){
+    // Ejemplo de grafo
     Grafo grafoEjemplo;
-    grafoEjemplo.lista_adyacencia = {{1, {2, 3}},{2, {4,5}},{3, {6}},{6, {7}},{4,{}},{5,{}},{7,{}}};
+    grafoEjemplo.lista_adyacencia = {{7, {5, 21, 75}},{5, {}},{21, {5,25}},{75, {6}},{25,{9}},{6,{8}},{9,{6}}, {8, {25}}};
     DFS dfs;
-    cout << "Cantidad de nodos visitados en DFS:\n" << dfs.buscar(1,7,grafoEjemplo)<<endl;
+    cout << "Cantidad de nodos visitados en DFS:\n" << dfs.buscar(7,9,grafoEjemplo)<<endl;
     BFS bfs;
-    cout << "Cantidad de nodos visitados en BFS:\n" << bfs.buscar(1,7,grafoEjemplo)<<endl;
+    cout << "Cantidad de nodos visitados en BFS:\n" << bfs.buscar(7,9,grafoEjemplo)<<endl;
     return 0;
 }
--- examen3-CI3641/c++_files/secuencia.cpp
+++ examen3-CI3641/c++_files/secuencia.cpp
@@ -1,3 +1,4 @@
+// librerias necesarias para el funcionamiento del programa
 #include <iostream>
 #include <stack>
 #include <queue>
@@ -5,16 +6,23 @@
 #include<string>
 using namespace std;
 
+// definicion de la clase generica Secuencia
 template <class T>
 class Secuencia{
+    // por defecto, una secuencia es un vector de tipo T
     vector<T> coleccion;
     public:
+        // Constructor
         Secuencia(){
             this->coleccion = vector<T>();
         };
+        // funcion que agrega un elemento a la secuencia. Se declara como virtual
+        // para que se sobreescriba esta funcion en las subclases de Secuencia
         virtual void agregar(T elemento){
             this->coleccion.push_back(elemento);
         };
+        // funcion que remueve el ultimo elemento de la secuencia. Se declara como virtual
+        // para que se sobreescriba esta funcion en las subclases de Secuencia
         virtual T remover(){
             if (! this->coleccion.empty()){
                 T elemento = this->coleccion.back();
@@ -23,10 +31,13 @@ class Secuencia{
             };
             throw "No se puede eliminar un elemento que no existe";
         };
+        // funcion que determina si la secuencia esta vacia. Se declara como virtual
+        // para que se sobreescriba esta funcion en las subclases de Secuencia
         virtual bool vacio(){
             return this->coleccion.empty();
         };
-        void imprimir(){
+        // funcion que imprime la secuencia
+        virtual void imprimir(){
             for(auto elem : this->coleccion){
                 cout << elem << " ";
             }
@@ -34,16 +45,21 @@ class Secuencia{
         }
 };
 
+// definicion de la clase derivada Pila
 template <class T>
 class Pila: public Secuencia<T>{
+    // por defecto, una pila es un stack de tipo T
     stack<T> coleccion;
     public:
+        // Constructor
         Pila(){
             this->coleccion = stack<T>();
         }
+        // Metodo que agrega un elemento a la pila.
         void agregar(T elemento){
             this->coleccion.push(elemento);
         };
+        // Metodo que remueve el ultimo elemento de la pila.
         T remover(){
             if (! this->coleccion.empty()){
                 T elemento = this->coleccion.top();
@@ -52,21 +68,39 @@ class Pila: public Secuencia<T>{
             };
             throw "No se puede eliminar un elemento que no existe";
         };
+        // Metodo que determina si la pila esta vacia.
         bool vacio(){
             return this->coleccion.empty();
         };
+
+        void imprimir(){
+            stack<T> copia = this->coleccion;
+
+            while (!copia.empty()){
+                T elem = copia.top();
+                copia.pop();
+
+                cout << elem << " ";
+            }
+            cout << endl;
+        }
 };
 
+// definicion de la clase derivada Cola
 template <class T>
 class Cola: public Secuencia<T>{
+    // por defecto, una cola es un queue de tipo T
     queue<T> coleccion;
     public:
+        // Constructor
         Cola(){
             this->coleccion = queue<T>();
         }
+        // Metodo que agrega un elemento a la cola.
         void agregar(T elemento){
             this->coleccion.push(elemento);
         };
+        // Metodo que remueve el primer elemento de la cola.
         T remover(){
             if (! this->coleccion.empty()){
                 T elemento = this->coleccion.front();
@@ -75,87 +109,137 @@ class Cola: public Secuencia<T>{
             };
             throw "No se puede eliminar un elemento que no existe";
         };
+        // Metodo que determina si la cola esta vacia.
         bool vacio(){
             return this->coleccion.empty();
         };
+
+        void imprimir(){
+            queue<T> copia = this->coleccion;
+
+            while (!copia.empty()){
+                T elem = copia.front();
+                copia.pop();
+                
+                cout << elem << " ";
+            }
+            cout << endl;
+        }
 };
 
+/********** PROGRAMA PRINCIPAL **********/
 int main(){
+    // Declacaciones de Secuencia
     Secuencia<int> S1;
-    S1.agregar(1); S1.agregar(2); S1.agregar(3);
-    S1.imprimir();
-    cout << S1.remover() << endl;
-    S1.imprimir();
-
     Secuencia<string> S2;
-    S2.agregar("hola"); S2.agregar("uwu"); S2.agregar("wiiii");
+    Secuencia<float> S3;
+
+    // Declaraciones de Pila
+    Pila<int> P1;
+    Pila<string> P2;
+    Pila<float> P3;
+
+    // Declaraciones de Cola
+    Cola<int> C1;
+    Cola<string> C2;
+    Cola<float> C3;
+
+    cout<<"DEFINIENDO SECUENCIAS\n"<<endl;
+
+    // agregar elementos a las secuencias
+    S1.agregar(1);
+    S1.agregar(2);
+    S1.agregar(3);
+    S1.agregar(4);
+    cout<<"Secuencia S1: ";
+    S1.imprimir();
+
+    S2.agregar("uno");
+    S2.agregar("dos");
+    S2.agregar("tres");
+    S2.agregar("cuatro");
+    cout<<"Secuencia S2: ";
     S2.imprimir();
-    cout << S2.remover() << endl;
+
+    S3.agregar(1.1);
+    S3.agregar(2.2);
+    S3.agregar(3.3);
+    S3.agregar(4.4);
+    cout<<"Secuencia S3: ";
+    S3.imprimir();
+
+    cout<<"\nELIMINANDO EL ULTIMO ELEMENTO DE LAS SECUENCIAS\n"<<endl;
+    // eliminar el ultimo elemento de las secuencias
+    cout<<"Para S1: "<<S1.remover()<<"\n"<<"S1: ";
+    S1.imprimir();
+    cout<<"Para S2: "<<S2.remover()<<"\n"<<"S2: ";
     S2.imprimir();
-    
-    cout << "Floats:\n";
-    cout << "   Pila:\n";
-    // Pila de floats
-    Pila<float> pilaFloats;
-    pilaFloats.agregar(1.5);
-    pilaFloats.agregar(2.2); 
-    pilaFloats.agregar(3.7);
+    cout<<"Para S3: "<<S3.remover()<<"\n"<<"S3: ";
+    S3.imprimir();
 
-    cout << "     " << pilaFloats.remover() << endl; // 3.7
-    cout << "     " << pilaFloats.remover() << endl; // 2.2
+    cout<<"\nDEFINIENDO PILAS\n"<<endl;
+    // agregar elementos a la pilas
+    P1.agregar(1);
+    P1.agregar(2);
+    P1.agregar(3);
+    P1.agregar(4);
+    cout<<"Pila P1: ";
+    P1.imprimir();
 
-    cout << "   Cola:\n";
-    // Cola de floats
-    Cola<float> colaFloats;
-    colaFloats.agregar(1.5);
-    colaFloats.agregar(2.2); 
-    colaFloats.agregar(3.7);
+    P2.agregar("uno");
+    P2.agregar("dos");
+    P2.agregar("tres");
+    P2.agregar("cuatro");
+    cout<<"Pila P2: ";
+    P2.imprimir();
 
-    cout << "     " << colaFloats.remover() << endl; // 1.5
-    cout << "     " << colaFloats.remover() << endl; // 2.2
+    P3.agregar(1.1);
+    P3.agregar(2.2);
+    P3.agregar(3.3);
+    P3.agregar(4.4);
+    cout<<"Pila P3: ";
+    P3.imprimir();
 
-    cout << "Enteros:\n";
-    cout << "   Pila:\n";
-    // Pila de enteros
-    Pila<int> pilaEnteros;
-    pilaEnteros.agregar(10);
-    pilaEnteros.agregar(20);
-    pilaEnteros.agregar(30);
+    cout<<"\nELIMINANDO EL ULTIMO ELEMENTO DE LAS PILAS\n"<<endl;
+    // eliminar el ultimo elemento de las pilas
+    cout<<"Para P1: "<<P1.remover()<<"\n"<<"P1: ";
+    P1.imprimir();
+    cout<<"Para P2: "<<P2.remover()<<"\n"<<"P2: ";
+    P2.imprimir();
+    cout<<"Para P3: "<<P3.remover()<<"\n"<<"P3: ";
+    P3.imprimir();
 
-    cout << "     " << pilaEnteros.remover() << endl; // 30
-    cout << "     " << pilaEnteros.remover() << endl; // 20
+    cout<<"\nDEFINIENDO COLAS\n"<<endl;
+    // agregar elementos a las colas
+    C1.agregar(1);
+    C1.agregar(2);
+    C1.agregar(3);
+    C1.agregar(4);
+    cout<<"Cola C1: ";
+    C1.imprimir();
 
-    cout << "   Cola:\n";
-    // Cola de enteros
-    Cola<int> colaEnteros;
-    colaEnteros.agregar(10);
-    colaEnteros.agregar(20);
-    colaEnteros.agregar(30);
+    C2.agregar("uno");
+    C2.agregar("dos");
+    C2.agregar("tres");
+    C2.agregar("cuatro");
+    cout<<"Cola C2: ";
+    C2.imprimir();
 
-    cout << "     " << colaEnteros.remover() << endl; // 10
-    cout << "     " << colaEnteros.remover() << endl; // 20
+    C3.agregar(1.1);
+    C3.agregar(2.2);
+    C3.agregar(3.3);
+    C3.agregar(4.4);
+    cout<<"Cola C3: ";
+    C3.imprimir();
 
-    cout << "Strings:\n";
-    cout << "   Pila:\n";
-    // Pila de strings
-    Pila<string> pilaStrings;
-    pilaStrings.agregar("hola");
-    pilaStrings.agregar("mundo");
-    pilaStrings.agregar("!");  
-
-    cout << "     " << pilaStrings.remover() << endl; // !
-    cout << "     " << pilaStrings.remover() << endl; // mundo
-
-    cout << "   Cola:\n";
-    // Cola de strings
-    Cola<string> colaStrings;
-    colaStrings.agregar("hola");
-    colaStrings.agregar("mundo");
-    colaStrings.agregar("!");  
-
-    cout << "     " << colaStrings.remover() << endl; // hola
-    cout << "     " << colaStrings.remover() << endl; // mundo
-
+    cout<<"\nELIMINANDO EL PRIMER ELEMENTO DE LAS COLAS\n"<<endl;
+    // eliminar el primer elemento de las colas
+    cout<<"Para C1: "<<C1.remover()<<"\n"<<"C1: ";
+    C1.imprimir();
+    cout<<"Para C2: "<<C2.remover()<<"\n"<<"C2: ";
+    C2.imprimir();
+    cout<<"Para C3: "<<C3.remover()<<"\n"<<"C3: ";
+    C3.imprimir();
 
     return 0;
 }
